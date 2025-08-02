8c2eddbdbe70c904dc92d674c730434583cb8d05
rafaelfcc <rfccrj@gmail.com>
2025-08-02 14:08:26 -0300

    Update UsuarioRepository.cs

--- Biblioteca.Repositories/Repositories/UsuarioRepository.cs
+++ Biblioteca.Repositories/Repositories/UsuarioRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Livraria.Data.Repositories
+namespace Biblioteca.Data.Repositories
 {
     public class UsuarioRepository : _BaseRepository<Usuario>
     {
